cce769f54c65fbaf75ae5cf54fe6cb2f7014956d
Alex <_@gmail.com>
2025-02-17 16:41:02 +0300

    fix: bug with FileNotFoundError

--- rag_chatbot/ui/ui.py
+++ rag_chatbot/ui/ui.py
@@ -208,7 +208,7 @@ class LocalChatbotUI:
             input_files = []
             for file_path in document:
                 dest = os.path.join(self._data_dir, file_path.split("/")[-1])
-                shutil.move(src=file_path, dst=dest)
+                shutil.copy(src=file_path, dst=dest)
                 input_files.append(dest)
             self._pipeline.store_nodes(input_files=input_files)
         else:
