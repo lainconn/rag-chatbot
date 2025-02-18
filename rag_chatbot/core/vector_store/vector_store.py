c2c7edd4e2f22a5cf47a94e687de47c3c3c0829f
Alex <_@gmail.com>
2025-02-18 16:33:14 +0300

    feat: add chromadb vectore store

--- rag_chatbot/core/vector_store/vector_store.py
+++ rag_chatbot/core/vector_store/vector_store.py
@@ -1,3 +1,5 @@
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import VectorStoreIndex
 from dotenv import load_dotenv
 from ...setting import RAGSettings
@@ -11,9 +13,18 @@ class LocalVectorStore:
         host: str = "host.docker.internal",
         setting: RAGSettings | None = None,
     ) -> None:
-        # TODO
-        # CHROMA VECTOR STORE
         self._setting = setting or RAGSettings()
+
+    def create_vector_store(self):
+        chroma_client = chromadb.PersistentClient(
+            path=self._setting.storage.persist_dir_storage
+        )
+        chroma_collection = chroma_client.create_collection(
+            self._setting.storage.collection_name
+        )
+
+        vectore_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        return vectore_store
 
     def get_index(self, nodes):
         if len(nodes) == 0:
