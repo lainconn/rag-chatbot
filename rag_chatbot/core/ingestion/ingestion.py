65d3c596064091f02dee9d0860a75b076d60d876
Alex <_@gmail.com>
2025-02-21 14:50:58 +0300

    fix: fix minor bugs

--- rag_chatbot/core/ingestion/ingestion.py
+++ rag_chatbot/core/ingestion/ingestion.py
@@ -25,6 +25,7 @@ class LocalDataIngestion:
         input_files: list[str],
         embed_model: Any | None = None,
     ) -> List[BaseNode]:
+        all_ids = self._node_store.client.get()["ids"]
         if len(input_files) == 0:
             return []
         splitter = SentenceSplitter.from_defaults(
@@ -36,31 +37,33 @@ class LocalDataIngestion:
         Settings.embed_model = embed_model or Settings.embed_model
         for input_file in tqdm(input_files):
             file_name = input_file.strip().split("/")[-1]
-            # if unique_id in self._node_store:
-            #     return self._node_store.get_nodes(node_ids=unique_id)
-            # else:
-            elements = partition(
-                filename=input_file,
-                languages=["rus", "eng"],
-                strategy="fast",
-                skip_infer_table_types=["jpg", "png", "heic"],
-            )
+            if set(self._ingested_file).intersection(all_ids):
+                print("Not Skipped!")
+                return self._node_store.get_nodes(node_ids=self._ingested_file)
+            else:
+                elements = partition(
+                    filename=input_file,
+                    languages=["rus", "eng"],
+                    strategy="fast",
+                    skip_infer_table_types=["jpg", "png", "heic"],
+                )
 
-            text = " "
-            for element in elements:
-                text += "\n\n" + element.text
+                text = " "
+                for element in elements:
+                    text += "\n\n" + element.text
 
-            document = Document(
-                text=text,
-                metadata={
-                    "file_name": file_name,
-                },
-            )
+                document = Document(
+                    text=text,
+                    metadata={
+                        "file_name": file_name,
+                    },
+                )
 
-            nodes = splitter([document], show_progress=True)
-            nodes = Settings.embed_model(nodes, show_progress=True)
-            all_ids = self._node_store.add(nodes)
-        return self._node_store.get_nodes(node_ids=all_ids)
+                nodes = splitter([document], show_progress=True)
+                nodes = Settings.embed_model(nodes, show_progress=True)
+                node_ids = self._node_store.add(nodes)
+                self._ingested_file.append(node_ids)
+        return self._node_store.get_nodes(node_ids=self._ingested_file[0])
 
     # def reset(self):
     #     self._node_store.delete_nodes()
@@ -78,7 +81,6 @@ class LocalDataIngestion:
 
     def get_ingested_nodes(self):
         return []
-        # return_nodes = []
         # for file in self._ingested_file:
         #     return_nodes.extend(self._node_store[file])
         # return return_nodes
